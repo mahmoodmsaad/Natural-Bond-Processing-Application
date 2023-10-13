f7fddab4b97e1f890c3b34be0ed892b1d53162fc
saad <146743997+mahmoodmsaad@users.noreply.github.com>
2023-10-13 12:08:43 +0500

    Update nbo.py

--- nbo.py
+++ nbo.py
@@ -4,27 +4,34 @@ import pandas as pd
 def load_data(file_path):
     return pd.read_csv(file_path)
 
-def filter_data(data, ignore_values, top_n, filter_type):
-    # Check if 'Orbital' column is present
-    if 'Orbital' not in data.columns:
-        st.warning("The 'Orbital' column is missing in the DataFrame.")
-        return
-
+def filter_data(data, ignore_values, selected_orbitals, top_n, ascending=True):
     # Ignore rows containing specified values
     for value in ignore_values:
-        data = data[~data['Orbital'].str.contains(value, na=False)]
+        data = data[data['Orbital'] != value]
 
-    # Display top or bottom kcal/mol values
-    if 'kcal/mol' in data.columns:
-        if filter_type == 'Top':
-            st.subheader(f"Top {top_n} Orbitals:")
-            result = data.nlargest(top_n, 'kcal/mol')
-        else:
-            st.subheader(f"Bottom {top_n} Orbitals:")
-            result = data.nsmallest(top_n, 'kcal/mol')
-        st.write(result)
-    else:
-        st.warning("The 'kcal/mol' column is missing in the DataFrame.")
+    # Filter by selected orbitals
+    if selected_orbitals:
+        data = data[data['Orbital'].isin(selected_orbitals)]
+
+    # Display maximum kcal/mol
+    max_kcal = data['kcal/mol'].max()
+    st.subheader(f"Maximum kcal/mol: {max_kcal}")
+
+    # Display information about specific orbitals
+    orbital_info = {}
+    for orbital in selected_orbitals:
+        orbital_count = data[data['Orbital'] == orbital].shape[0]
+        orbital_info[orbital] = {
+            'Count': orbital_count,
+            'Rows': data[data['Orbital'] == orbital].index.tolist()
+        }
+
+    st.subheader("Orbital Information:")
+    st.write(orbital_info)
+
+    # Sort by kcal/mol and display top N
+    sorted_data = data.sort_values(by='kcal/mol', ascending=ascending)
+    return sorted_data.head(top_n)
 
 def main():
     st.title("Orbital Energy Analyzer")
@@ -40,14 +47,24 @@ def main():
         st.write(data)
 
         # Ignore specified orbitals
-        ignore_values = st.multiselect("Ignore Orbitals:", ['RY*', 'CR*', 'LP*'])
+        ignore_values = st.multiselect("Ignore Orbitals:", ['RY*', 'CR', 'LP'])
+
+        # Select specific orbitals
+        all_orbitals = list(data['Orbital'].unique())
+        selected_orbitals = st.multiselect("Select Specific Orbitals (Optional):", all_orbitals)
 
         # Filter by top or bottom energies
         filter_type = st.radio("Select Filter Type:", ["Top", "Bottom"])
         top_n = st.number_input("Number of Orbitals to Display:", min_value=1, max_value=len(data), value=10)
 
-        # Apply filters and display results
-        filter_data(data, ignore_values, top_n, filter_type)
+        if filter_type == "Top":
+            st.subheader(f"Top {top_n} Orbitals:")
+            result = filter_data(data, ignore_values, selected_orbitals, top_n)
+        else:
+            st.subheader(f"Bottom {top_n} Orbitals:")
+            result = filter_data(data, ignore_values, selected_orbitals, top_n, ascending=False)
+
+        st.write(result)
 
 if __name__ == "__main__":
     main()
